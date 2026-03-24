acbf1874714a47ac9a3108e746464d8983e42a95
YUVA KUMARAN R <yuvakumaran.37@gmail.com>
2026-03-24 22:11:45 +0530

    Problem

---
+++ Day5/StringBuilder.java
@@ -0,0 +1,10 @@
+public class StringBuilder {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("Hello");
+        System.out.println(sb);
+        System.out.println(sb.toString());
+        sb.append(" World");
+        System.out.println(sb);
+        
+    }
+}
