3d451f964f2b171354a0503747c09e9205352de4
YUVA KUMARAN R <yuvakumaran.37@gmail.com>
2026-04-05 18:49:29 +0530

    Problem

---
+++ Day11 - Task/Range.java
@@ -0,0 +1,13 @@
+public class Range {
+    public static void main(String[] args) {
+        Integer a=127;
+        Integer obj=127;
+        System.out.println(a==obj);//true
+        Integer b=200;
+        Integer obj1=200;
+        System.out.println(b==obj1);//false
+        Float k=127f;
+        Float m=127f;
+        System.out.println(k==m);//false 
+    }
+}
