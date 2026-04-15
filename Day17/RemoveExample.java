785ece4c5570e6c7609ff40e8aba3fe0e76c17ab
YUVA KUMARAN R <yuvakumaran.37@gmail.com>
2026-04-15 08:55:40 +0530

    Create RemoveExample.java

---
+++ Day17/RemoveExample.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoveExample{
+     public static void main(String[] args) {
+        List<Integer> list= new ArrayList<>();
+        list.add(15);
+        list.add(20);
+        list.add(26);
+        list.add(30);
+        System.out.println(list);
+        for(Integer i:list){
+            list.remove(i);
+        }
+        System.out.println(list);
+    }
+}
