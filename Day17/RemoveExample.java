dc73d91904bf50f0cc39e1b360f99e208c4153f2
YUVA KUMARAN R <yuvakumaran.37@gmail.com>
2026-04-15 09:01:19 +0530

    Problem

--- Day17/RemoveExample.java
+++ Day17/RemoveExample.java
@@ -6,7 +6,7 @@ public class RemoveExample{
         List<Integer> list= new ArrayList<>();
         list.add(15);
         list.add(20);
-        list.add(26);
+        list.add(26); 
         list.add(30);
         System.out.println(list);
         for(Integer i:list){
