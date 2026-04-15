e6ba7374fce31b9757c9053b5ffa505408fce2a4
YUVA KUMARAN R <yuvakumaran.37@gmail.com>
2026-04-15 08:51:24 +0530

    Problem

---
+++ Day17/ComparableImplementation.java
@@ -0,0 +1,30 @@
+package Day_17.Concepts_Code;
+import java.util.*;
+
+    class Student implements Comparable<Student>{
+        int rollNo;
+        String name;
+        Student(int rollNo,String name){
+            this.rollNo=rollNo;
+            this.name=name;
+        }
+        public int compareTo(Student s){
+            return s.rollNo - this.rollNo;
+        }
+    }
+
+public class ComparableImplementation {      
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(3,"yuva"));
+        students.add(new Student(1,"vig"));
+        students.add(new Student(2,"siva"));
+
+        Collections.sort(students);
+        for(Student s: students){
+            System.out.println(s.rollNo + " " + s.name);
+        }
+        
+    }
+    
+}
